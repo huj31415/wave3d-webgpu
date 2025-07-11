284272e9ab02ba8e76e060855d2791dbf129d9eb
huj31 <huj31415@gmail.com>
2025-07-11 19:49:59 +0800

    Bugfixes and documentation

--- gui.js
+++ gui.js
@@ -10,6 +10,11 @@ class GUI {
   parent;
   toggle;
 
+  /**
+   * Creates a GUI object
+   * @param {String} title Title of the application
+   * @param {HTMLElement} appendAfter Element to add the GUI after
+   */
   constructor(title = "", appendAfter) {
     const parentDiv = document.createElement("div");
     parentDiv.id = "controls"
@@ -40,6 +45,13 @@ class GUI {
     });
   }
 
+  /**
+   * Adds a group to the GUI
+   * @param {String} id id of the group, for adding other IO under it
+   * @param {String} title Title of the group
+   * @param {String} html Plain HTML to add to to the group
+   * @returns The newly created group
+   */
   addGroup(id, title, html = "") {
     const group = document.createElement("div");
     group.className = "control-group";
@@ -57,6 +69,12 @@ class GUI {
     return group;
   }
 
+  /**
+   * Creates half-width groups under a specified group
+   * @param {String} id1 id of the left group
+   * @param {String} id2 id of the right group
+   * @param {String} group id of the group to add this under
+   */
   addHalfWidthGroups(id1, id2, group = "parent") {
     const group1 = document.createElement("span");
     group1.className = "halfWidth";
@@ -71,6 +89,13 @@ class GUI {
     this.groups[id2] = group2;
   }
 
+  /**
+   * Adds a string output to the gui
+   * @param {String} id id of the output
+   * @param {String} label Label (prefix) of the output value, a colon will be added
+   * @param {String} suffix String appended after the output value, eg. units
+   * @param {String} group id of the group to add this under
+   */
   addStringOutput(id, label = "", suffix = "", group = "parent") {
     const span = document.createElement("span");
     span.id = id;
@@ -83,6 +108,14 @@ class GUI {
     this.io[id] = (val) => (span.innerText = val);
   }
 
+  /**
+   * Adds a numeric output to the gui
+   * @param {String} id id of the output
+   * @param {String} label Label (prefix) of the output value, a colon will be added
+   * @param {String} suffix String appended after the output value, eg. units
+   * @param {Number} floatPrecision Precision of the printed value, set to 0 for integer
+   * @param {String} group id of the group to add this under
+   */
   addNumericOutput(id, label = "", suffix = "", floatPrecision = 2, group = "parent") {
     // const line = document.createElement("div");
     const span = document.createElement("span");
@@ -96,6 +129,15 @@ class GUI {
     this.io[id] = (val) => (span.innerText = floatPrecision == 0 ? parseInt(val) : parseFloat(val).toFixed(floatPrecision));
   }
 
+  /**
+   * Creates a multidimensional numeric output
+   * @param {Array<String>} ids Array of ids for each dimension of the output
+   * @param {String} label Label of the output value, a colon will be added
+   * @param {String} suffix String appended after the output value, eg. units
+   * @param {String} separator String used to separate the individual numbers
+   * @param {Number} floatPrecision Precision of the printed value, set to 0 for integer
+   * @param {String} group id of the group to add this under
+   */
   addNDimensionalOutput(ids, label = "", suffix = "", separator = ", ", floatPrecision = 2, group = "parent") {
     this.groups[group].append(label + ": ");
 
@@ -109,7 +151,23 @@ class GUI {
     this.groups[group].appendChild(document.createElement("br"));
   }
 
+  /**
+   * Adds a numeric input
+   * @param {String} id id of the input. A container div is created with id `${id}-container`
+   * @param {Boolean} range Whether to use a range input or a plain number input
+   * @param {String} label Label of the input, a colon and the value will be added if range is true
+   * @param {Number} min Min value
+   * @param {Number} max Max value
+   * @param {Number} step Step size
+   * @param {Number} value Value at initialization
+   * @param {Number} floatPrecision Precision of the output display
+   * @param {String} group id of the group to add this under
+   * @param {Function} oninput Callback function of the new value to run on user input
+   */
   addNumericInput(id, range = true, label, min, max, step, value = (min + max) / 2, floatPrecision = 2, group = "parent", oninput) {
+    const container = document.createElement("div");
+    container.id = `${id}-container`;
+
     const input = document.createElement("input");
     input.type = range ? "range" : "number";
     input.id = id;
@@ -118,29 +176,48 @@ class GUI {
     input.step = step;
     input.value = value;
 
-    const valueSpan = document.createElement("span");
-    valueSpan.id = id + "Value";
-    valueSpan.innerText = floatPrecision == 0 ? parseInt(value) : parseFloat(value).toFixed(2);
-
     const labelEl = document.createElement("label");
     labelEl.setAttribute("for", id);
-    labelEl.innerText = `${label}: `;
-    labelEl.appendChild(valueSpan);
 
-    this.groups[group].appendChild(input);
-    this.groups[group].appendChild(labelEl);
-    this.groups[group].appendChild(document.createElement("br"));
+    let valueSpan;
+
+    if (range) {
+      labelEl.innerText = `${label}: `;
+      valueSpan = document.createElement("span");
+      valueSpan.id = id + "Value";
+      valueSpan.innerText = floatPrecision == 0 ? parseInt(value) : parseFloat(value).toFixed(2);
+      labelEl.appendChild(valueSpan);
+    } else {
+      labelEl.innerText = label;
+    }
+
+
+    container.appendChild(input);
+    container.appendChild(labelEl);
+
+    this.groups[group].appendChild(container);
 
     this.io[id] = input;
     // this.io[id + "Value"] = valueSpan;
 
     input.addEventListener("input", () => {
-      valueSpan.innerText = floatPrecision == 0 ? parseInt(input.value) : parseFloat(input.value).toFixed(floatPrecision);
+      if (valueSpan) valueSpan.innerText = floatPrecision == 0 ? parseInt(input.value) : parseFloat(input.value).toFixed(floatPrecision);
       if (oninput) oninput(input.value);
     });
   }
 
+  /**
+   * Adds a checkbox
+   * @param {String} id id of the input. A container div is created with id `${id}-container`
+   * @param {String} label Label of the input
+   * @param {Boolean} startChecked Whether to initialize checked
+   * @param {String} group id of the group to add this under
+   * @param {Function} onclick Callback function of checked state to run on user input
+   */
   addCheckbox(id, label, startChecked = false, group = "parent", onclick) {
+    const container = document.createElement("div");
+    container.id = `${id}-container`;
+
     const input = document.createElement("input");
     input.type = "checkbox";
     input.id = id;
@@ -150,19 +227,32 @@ class GUI {
     labelEl.setAttribute("for", id);
     labelEl.innerText = label;
 
-    this.groups[group].appendChild(input);
-    this.groups[group].appendChild(labelEl);
-    this.groups[group].appendChild(document.createElement("br"));
+    container.appendChild(input);
+    container.appendChild(labelEl);
+
+    this.groups[group].appendChild(container);
 
     this.io[id] = input;
 
     if (onclick != null) input.addEventListener("click", () => onclick(input.checked));
   }
 
-  addDropdown(id, label, options = [], group = "parent", visibilityMap = {}) {
+  /**
+   * Adds a dropdown selector
+   * @param {String} id id of the input. A container div is created with id `${id}-container`
+   * @param {String} label Label
+   * @param {Array<String>} options Array of string options which are also values
+   * @param {String} group id of the group to add this under
+   * @param {Object} visibilityMap Object of format `"option": ["id1", "id2"]` of input ids to display when the option is selected
+   * @param {Function} onChange Callback function of the currently selected value to run on user input
+   */
+  addDropdown(id, label, options = [], group = "parent", visibilityMap = {}, onChange = null) {
+    const container = document.createElement("div");
+    container.id = `${id}-container`;
+
     const labelEl = document.createElement("label");
     labelEl.setAttribute("for", id);
-    labelEl.innerText = label + ": ";
+    labelEl.innerText = label;
 
     const select = document.createElement("select");
     select.id = id;
@@ -174,26 +264,43 @@ class GUI {
       select.appendChild(option);
     });
 
-    labelEl.appendChild(select);
-    this.groups[group].insertBefore(select, this.io[Object.entries(visibilityMap)[0]]);
-    select.after(document.createElement("br"));
+    container.appendChild(select);
+    container.appendChild(labelEl);
 
+    const firstInput = document.getElementById(`${Object.entries(visibilityMap)[0][1][0]}-container`);
+    this.groups[group].insertBefore(container, firstInput);
     this.io[id] = select;
 
-    // Visibility logic for additional inputs
-    select.addEventListener("change", () => {
-      Object.entries(visibilityMap).forEach(([val, elements]) => {
-        elements.forEach(el => {
-          if (typeof el === 'string') el = this.io[el];
-          if (el) el.style.display = (select.value === val) ? "" : "none";
-        });
+    // Build reverse visibility map: inputId → [values]
+    const inputMap = {};
+    Object.entries(visibilityMap).forEach(([val, ids]) => {
+      ids.forEach(inputId => {
+        if (!inputMap[inputId]) inputMap[inputId] = [];
+        inputMap[inputId].push(val);
       });
     });
 
-    // Initialize visibility based on default
-    select.dispatchEvent(new Event("change"));
+    const updateVisibility = () => {
+      const selected = select.value;
+      Object.entries(inputMap).forEach(([inputId, allowedVals]) => {
+        const el = document.getElementById(`${inputId}-container`);
+        if (el) el.style.display = allowedVals.includes(selected) ? "" : "none";
+      });
+      if (onChange) onChange(selected);
+    };
+
+    select.addEventListener("change", updateVisibility);
+    updateVisibility(); // initialize
   }
 
+  /**
+   * Adds a button
+   * @param {String} id id of the input
+   * @param {String} label Label
+   * @param {Boolean} fullWidth Whether to add a half or full width button
+   * @param {String} group id of the group to add this under
+   * @param {Function} onclick Callback function to run when the button is clicked
+   */
   addButton(id, label, fullWidth = false, group = "parent", onclick) {
     const button = document.createElement("button");
     if (fullWidth) button.classList.add("fullwidth");
@@ -206,8 +313,17 @@ class GUI {
     this.io[id] = button;
   }
 
-  addRadioOptions(name, options = [], defaultValue = null, group = "parent") {
+  /**
+   * Adds a set of radio options
+   * @param {String} name name of the input
+   * @param {Array<String>} options Array of string options which are also the values
+   * @param {String} defaultValue Value to be selected at initialization
+   * @param {String} group id of the group to add this under
+   * @param {Function} onChange Callback function of the selected value to run on user input
+   */
+  addRadioOptions(name, options = [], defaultValue = null, group = "parent", onChange) {
     const container = document.createElement("div");
+    container.id = `${name}-container`;
 
     options.forEach((value) => {
       const input = document.createElement("input");
@@ -216,6 +332,10 @@ class GUI {
       input.value = value;
       input.id = `${name}_${value}`;
       if (value === defaultValue) input.checked = true;
+
+      input.addEventListener("change", () => {
+        if (onChange) onChange(value);
+      });
 
       const labelEl = document.createElement("label");
       labelEl.setAttribute("for", input.id);
